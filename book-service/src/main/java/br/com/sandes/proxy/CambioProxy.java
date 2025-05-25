6c1f5e41017616e95de731bb4ebd83beaa6f4f52
Mateus Sandes <mateussandes@hotmail.com.br>
2025-05-25 00:06:41 -0300

    Removdo a url do cambio da classe

--- book-service/src/main/java/br/com/sandes/proxy/CambioProxy.java
+++ book-service/src/main/java/br/com/sandes/proxy/CambioProxy.java
@@ -5,11 +5,11 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "cambio-service", url = "localhost:8000")
+@FeignClient(name = "cambio-service")
 public interface CambioProxy {
 
     @GetMapping(value = "/cambio-service/{amount}/{from}/{to}")
-    public Cambio getCambio(
+    Cambio getCambio(
             @PathVariable("amount") Double amount,
             @PathVariable("from") String from,
             @PathVariable("to") String to);
