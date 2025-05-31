0e0e86af1fb31b6714daf945e16ecca10af1fa8e
Mateus Sandes <mateussandes@hotmail.com.br>
2025-05-31 16:32:50 -0300

    Criado o ednpoint de fallback no casdo de erros na api

--- book-service/src/main/java/br/com/sandes/controllers/FoobBarController.java
+++ book-service/src/main/java/br/com/sandes/controllers/FoobBarController.java
@@ -1,19 +1,23 @@
 package br.com.sandes.controllers;
 
 import io.github.resilience4j.bulkhead.annotation.Bulkhead;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "Foo bar")
 @RestController
 @RequestMapping("book-service")
 public class FoobBarController {
 
-    private Logger logger = LoggerFactory.getLogger(FoobBarController.class);
+    private final Logger logger = LoggerFactory.getLogger(FoobBarController.class);
 
     @GetMapping("/foo-bar")
+    @Operation(summary = "Foo bar")
     //@Retry(name = "default", fallbackMethod = "fallBackMethod")
     //@CircuitBreaker(name = "default", fallbackMethod = "fallBackMethod")
     //@RateLimiter(name = "default") define uma quantidade de requisicoes que o endpoint anotado pode receber
